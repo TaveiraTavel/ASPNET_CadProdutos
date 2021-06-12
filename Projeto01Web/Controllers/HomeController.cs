ac3330f3384f76b157ab3881b96d0749b8e9b9e4
Mateus Taveira <mateus.staveira@gmail.com>
2021-06-11 21:08:36 -0300

    Add project files.

---
+++ Projeto01Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto01Web.Models;
+
+namespace Projeto01Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var produto = new Produto();
+            return View(produto);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", produto);
+            }
+            return View(produto);
+        }
+
+        public ActionResult Resultado(Produto produto)
+        {
+            return View(produto);
+        }
+    }
+}
---
+++ Projeto01Web/Models/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto01Web.Models
+{
+    public class Produto
+    {
+        // Código
+        [Required(ErrorMessage = "* Campo obrigatório")]
+        public int Codigo { get; set; }
+
+        // Nome
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "O nome do produto deve ter entre 5 e 50 caracteres")]
+        [Required(ErrorMessage = "* Campo obrigatório")]
+        public string Nome { get; set; }
+
+        // Descrição
+        [Required(ErrorMessage = "* Campo obrigatório")]
+        public string Descricao { get; set; }
+
+        // Peso
+        [Required(ErrorMessage = "* Campo obrigatório")]
+        public decimal Peso { get; set; }
+
+        // Categoria
+        [Required(ErrorMessage = "* Campo obrigatório")]
+        public string Categoria { get; set; }
+
+        // ValorUnit
+        [Required(ErrorMessage = "* Campo obrigatório")]
+        public decimal ValorUnit { get; set; }
+    }
+}
